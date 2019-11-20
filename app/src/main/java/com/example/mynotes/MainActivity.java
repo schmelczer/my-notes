1e52c3ea9865ad5e96b1437ca6bfcf93a31b4522
Schmelczer András <schmelczerandras@gmail.com>
2019-11-20 13:51:36 +0100

    Add skeleton

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mynotes;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -41,8 +42,8 @@ public class MainActivity extends AppCompatActivity implements NoteAdapter.OpenN
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+                startActivity(intent);
             }
         });
 
